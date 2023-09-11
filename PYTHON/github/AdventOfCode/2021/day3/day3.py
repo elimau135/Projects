73696c279c295d74dbda4bde6eb3b679b5e227f9
elimau135 <96752355+elimau135@users.noreply.github.com>
2023-09-11 17:37:54 +0200

    Update day3.py

--- PYTHON/github/AdventOfCode/2021/day3/day3.py
+++ PYTHON/github/AdventOfCode/2021/day3/day3.py
@@ -39,7 +39,6 @@ def convertBinary():
 # are beneath or above 500 it is the most common in this column
 
          
-    print(B)
 
 def mostValue():
 
@@ -97,56 +96,7 @@ def mostValue():
 
 #----------------------------------#----------------------------------#----------------------------------
 
-#first we calculate the most commen value also known as gamma
-# A = []
-# gamma = []
-# print("Hello")
-#
-# with open("AOC_day3.txt", "r") as f:
-#     for line in f:
-#         A.append(line.strip())
-#
-# for i in range(len(A[0])):
-#     count_0 = 0
-#     count_1 = 0
-#     for binary_string in A:
-#         if binary_string[i]=="0":
-#             count_0 += 1
-#         elif binary_string[i]=="1":
-#             count_1 += 1
-#     commonBit = "0" if count_0 > count_1 else "1"
-#     gamma.append(commonBit)
-#     gammaString = "".join(gamma)
-#
-#
-# #then we convert gamma into epsilon. With this we create a new value
-# epsilon = list(A) 
-# for i in range(0, len(A)):
-#     if epsilon[i] == "1":
-#         epsilon[i]= "0"
-#     else:
-#         epsilon[i]="1"
-#         epsilon = ''.join(epsilon)
-#
-#
-# #after this we convert the binary numbers gamma and epsilon into decimals 
-# sum = 0
-# for i in range(len(gamma)-1 , -1, -1):
-#     if A[i]=="1":
-#         sum += 2** (len(gamma)-1-i)
-# gammaDecimal = sum
-#
-# sum = 0
-# for i in range(len(epsilon)-1 , -1, -1):
-#     if value[i]=="1":
-#         sum += 2** (len(epsilon)-1-i)
-# epsilonDecimal = sum
-#
-# #calculating the result through gamma*epsilon
-# result = gammaDecimal * epsilonDecimal
-# print(result)
 
-#ChatGPT corrected
 
 A = []
 gamma = []
@@ -212,84 +162,94 @@ ogr = 0 #oxygen generator rating -> most common value
 csr = 0 #C02 scrubber rating -> least common value
 lsr = 0 #result -> lsr = ogr * csr
 
-# while len(binaryValue) != 1:
-#     for i in range (len(A[0])):
-#         count0=0
-#         count1=0
-#         for binaryString in A:
-#             if binaryString[i] == "0":
-#                 count0+=1
-#             elif binaryString[i]=="1":
-#                 count1+=1
-#         mostCommonBit = "0" if count0 > count1 else "1"
-#         index = int(mostCommonBit)
-#         binaryValue.append(binaryString[index])
-
-
-
-# while len(binaryValue) != 1:
-#     for i in range (len(A[0])):
-#         count0=0
-#         count1=0
-#         for binaryString in A:
-#             if binaryString[i] == "0":
-#                 count0+=1
-#             elif binaryString[i]=="1":
-#                 count1+=1
-#         mostCommonBit = "0" if count0 > count1 else "1"
-#         index = int(mostCommonBit)
-#         if binaryString[i]== binaryString[index]:
-#             binaryValue.append(binaryString[i])
-#
-# print(binaryValue)
-
-
-
-
-
-
-
-
-
-
-
 A = []
-
+list1=[]
 # Read binary strings from the file "testvalues.txt"
-with open("testvalues.txt", "r") as f:
+with open("AOC_day3.txt", "r") as f:
     for line in f:
         A.append(line.strip())
 
-binaryValue = []
-
-while len(binaryValue) != 1:
-    common_bits = []  # List to store common bits at each index
+for i in range(len(A[0])):
+    list1.clear()
+    count0 = 0 
+    count1 = 0 
+    commonValue = 0 
+    for j in A:
+        if j[i]== "1":
+            count1+=1
+        elif j[i]=="0":
+            count0+=1
+    commonValue = '1' if count1 >= count0 else '0'
     
-    for i in range(len(A[0])):
-        count_0 = 0
-        count_1 = 0
-        for binaryString in A:
-            if binaryString[i] == "0":
-                count_0 += 1
-            elif binaryString[i] == "1":
-                count_1 += 1
-        mostCommonBit = "0" if count_0 > count_1 else "1"
+    for j in A:
+        if j[i] == commonValue:
+            list1.append(j)
         
-        # Check if there is more than one common bit at this index
-        if count_0 > 0 and count_1 > 0:
-            common_bits.append(mostCommonBit)
-        
-    # If there are common bits at any index, append them to binaryValue
-    if common_bits:
-        binaryValue.extend(common_bits)
-    else:
-        # If no common bits found, pick the most common bit from the last index
-        count_0 = sum(1 for binaryString in A if binaryString[-1] == "0")
-        count_1 = len(A) - count_0
-        mostCommonBit = "0" if count_0 > count_1 else "1"
-        binaryValue.append(mostCommonBit)
+    A = list1.copy()
+   
+    
 
-print(binaryValue)
+    if len(A) == 1:
+        print(A)
+        value1 = A[0]
+        break
+
+sum1 = 0
+for i in range(len(value1)-1 , -1, -1):
+    if value1[i]=="1":
+        sum1 += 2** (len(value1)-1-i)
+print(sum1)
+ogr = sum1
+    
+    
+#least common value
+    
+B = []
+list2 = []
+uncommonValue = 0 
+
+with open('AOC_day3.txt', 'r') as f:
+    for line in f:
+        B.append(line.strip())
+        
+for i in range(len(B[0])):
+    list2.clear()
+    count0 = 0 
+    count1 = 0
+    commonValue = 0 
+    for j in B:
+        if j[i] == '1':
+            count1 += 1
+        elif j[i] == '0':
+            count0 += 1
+    uncommonValue = '0' if count1 >count0 or count1 == count0 else '1'
+    
+    
+    for j in B:
+        if j[i] == uncommonValue:
+            list2.append(j)
+    
+    B = list2.copy()
+
+    if len(B) == 1:
+        print(B)
+        value2 = B[0]
+        break
+    
+
+sum2 = 0
+for i in range(len(value2)-1 , -1, -1):
+    if value2[i]=="1":
+        sum2 += 2** (len(value2)-1-i)
+print(sum2)
+csr = sum2
+
+
+
+
+lsr = ogr * csr
+
+print(lsr)
 
 
 
